9c81a3005f55877b109e4869d7e11c6b77482315
Rathish <rathishvbr@gmail.com>
2024-09-29 14:20:50 +0200

    Adding ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/home';
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/home'
+import { lightTheme, darkTheme } from './utils/theme'
+import { ThemeProvider, CssBaseline, IconButton } from '@mui/material'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode)
+  }
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <div style={{ padding: '16px' }}>
+        <IconButton aria-label="Toggle Theme" onClick={toggleTheme}>
+          {isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
+        </IconButton>
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/utils/theme.js
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material/styles';
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#ff4081',
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+});
+
+export { lightTheme, darkTheme };
